b6ab367b6a32c417056b7e4e8a7889375414e204
Bradley J. Spaulding <brad.spaulding@gmail.com>
2018-08-13 11:02:50 -0700

    Updated test package for new rollup

--- test/rollup.config.js
+++ test/rollup.config.js
@@ -1,7 +1,10 @@
 import image from '../src/index';
 
 export default {
-	entry: './index',
-	dest: './output/bundle.js',
+	input: './index',
+	output: {
+		file: './output/bundle.js',
+		format: 'cjs'
+	},
 	plugins: [image()]
 };
