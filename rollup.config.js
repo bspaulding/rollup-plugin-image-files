74ea067333994ea638421e9f5d6c3759be071705
Bradley J. Spaulding <brad.spaulding@gmail.com>
2019-08-27 16:46:47 -0700

    ran prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
 export default {
 	input: 'src/index.js',
-	output: [{
-		file: 'dist/index.cjs.js',
-		format: 'cjs'
-	}, {
-		file: 'dist/index.es.js',
-		format: 'es'
-	}]
+	output: [
+		{
+			file: 'dist/index.cjs.js',
+			format: 'cjs'
+		},
+		{
+			file: 'dist/index.es.js',
+			format: 'es'
+		}
+	]
 };
